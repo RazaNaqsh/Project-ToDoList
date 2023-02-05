3740707c1b3802194bdb4c783e18e59cb13b25ce
RazaNaqsh <owaisraza1704@gmail.com>
2023-02-05 09:17:09 +0530

    Task submittion updates array

--- src/task.js
+++ src/task.js
@@ -1,15 +1,39 @@
+class TaskCreator {
+	constructor(title) {
+		this.title = title;
+	}
+}
+const Inbox = [];
+const taskModal = document.getElementById("createTaskModal");
+const addTaskBtn = document.getElementById("addTask");
+
 function newTaskModal() {
-	const taskModal = document.getElementById("createTaskModal");
 	const list = document.querySelector("#lists");
 	list.style.opacity = "0.7";
 	taskModal.style.display = "flex";
 }
+function submitTaskData() {
+	const submitTaskBtn = document.getElementById("createTaskBtn");
+	submitTaskBtn.addEventListener("click", (e) => {
+		e.preventDefault();
+		const taskTitle = document.getElementById("taskTitle").value;
+		if (taskTitle !== "") {
+			const taskItem = new TaskCreator(taskTitle);
+			Inbox.push(taskItem);
+			document.getElementById("taskTitle").value = "";
+			addTaskBtn.removeEventListener("click", createTask);
+			addTaskBtn.addEventListener("click", createTask);
+			console.log(Inbox);
+
+			taskModal.style.display = "none";
+		}
+	});
+}
 function createTask() {
 	newTaskModal();
+	submitTaskData();
 }
 
 export default function task() {
-	const Inbox = [];
-	const addTaskBtn = document.getElementById("addTask");
 	addTaskBtn.addEventListener("click", createTask);
 }

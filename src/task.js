0a591844f2889c9325bfe1feabfb12a5493f5e39
RazaNaqsh <owaisraza1704@gmail.com>
2023-02-05 14:08:05 +0530

    Function to add tasks to screen

--- src/task.js
+++ src/task.js
@@ -3,37 +3,78 @@ class TaskCreator {
 		this.title = title;
 	}
 }
-const Inbox = [];
+const Inbox = [
+	{
+		title: "TestTask1",
+	},
+	{
+		title: "testTask2",
+	},
+];
+
 const taskModal = document.getElementById("createTaskModal");
 const addTaskBtn = document.getElementById("addTask");
+const list = document.querySelector("#lists");
 
+function domFactory(item) {
+	const divItem = document.createElement("div");
+	divItem.classList.add("listContainer__listItem");
+
+	const inputCheck = document.createElement("input");
+	inputCheck.type = "checkbox";
+
+	const para = document.createElement("p");
+	para.textContent = `${item.title}`;
+
+	divItem.append(inputCheck, para);
+	list.append(divItem);
+}
+
+function displayToDom() {
+	Inbox.forEach((item) => {
+		domFactory(item);
+	});
+}
+
+function resetScreen() {
+	list.style.opacity = "1";
+	document.getElementById("taskTitle").value = "";
+
+	refreshEvent();
+
+	taskModal.style.display = "none";
+}
+function addToArray(e) {
+	e.preventDefault();
+	const taskTitle = document.getElementById("taskTitle").value;
+	if (taskTitle !== "") {
+		const taskItem = new TaskCreator(taskTitle);
+		Inbox.push(taskItem);
+		resetScreen();
+		domFactory(taskItem);
+	}
+}
 function newTaskModal() {
-	const list = document.querySelector("#lists");
 	list.style.opacity = "0.7";
 	taskModal.style.display = "flex";
 }
+
 function submitTaskData() {
 	const submitTaskBtn = document.getElementById("createTaskBtn");
-	submitTaskBtn.addEventListener("click", (e) => {
-		e.preventDefault();
-		const taskTitle = document.getElementById("taskTitle").value;
-		if (taskTitle !== "") {
-			const taskItem = new TaskCreator(taskTitle);
-			Inbox.push(taskItem);
-			document.getElementById("taskTitle").value = "";
-			addTaskBtn.removeEventListener("click", createTask);
-			addTaskBtn.addEventListener("click", createTask);
-			console.log(Inbox);
-
-			taskModal.style.display = "none";
-		}
-	});
+	submitTaskBtn.addEventListener("click", addToArray);
 }
+
 function createTask() {
 	newTaskModal();
 	submitTaskData();
 }
 
+function refreshEvent() {
+	addTaskBtn.removeEventListener("click", createTask);
+	addTaskBtn.addEventListener("click", createTask);
+}
+
 export default function task() {
 	addTaskBtn.addEventListener("click", createTask);
+	displayToDom();
 }
